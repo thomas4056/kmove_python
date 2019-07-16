2f03ae158ebee567ba2553273d0772e2222c81a2
thomas4056 <mig00051@naver.com>
2019-07-17 00:14:12 +0900

    20190716 과제

--- basic/randam.py
+++ basic/randam.py
@@ -1,6 +1,16 @@
 import random
 
-#sorted() 내장함수 이용: 믄자열을 받아서 정렬시켜준다.
+
+for a in range(0,5):
+    b=[0,0,0,0,0,0]
+    for c in range(0,6):
+        d=0
+        while (d in b):
+            d=random.randint(1,45)
+        b[c]=d
+    print("로또:"+ str(sorted(b)))
+
+#sorted() 내장함수 이용: 문자열을 받아서 정렬시켜준다.
 #b=[]
 #while True:
     #a=(random.randint(1,45))
@@ -21,12 +31,3 @@ import random
 #    b.append(a)
 #    if len(b)==6:
 #       print('로또%d='%(d),b)
-
-for a in range(0,5):
-    b=[0,0,0,0,0,0]
-    for c in range(0,6):
-        d=0
-        while (d in b):
-            d=random.randint(1,45)
-        b[c]=d
-    print("로또:"+ str(sorted(b)))
