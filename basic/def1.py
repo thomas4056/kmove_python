6c26f3f465872f7a1cbccc6cf819a1ec14366813
thomas4056 <52906770+thomas4056@users.noreply.github.com>
2019-07-23 17:40:25 +0900

    20190723

--- basic/def1.py
+++ basic/def1.py
@@ -2,57 +2,34 @@ import re, pickle, os
 custlist=[]
 page=-1
 
-def exe(choice):
-        if choice=='I':
-            insertData()
-            
-        elif choice=='C':
-            curSearch()
-        
-        elif choice=='P':
-            preSearch()
-
-        elif choice=='N':
-            nextSearch()
-
-        elif choice=='U':
-            updateData()
-        
-        elif choice=='D':
-            deleteData()
-        
-        elif choice=='Q':
-            saveData()
-            quit()
-
 def insertData():
-        customer={'name':'','sex':'',"email":'',"birthyear":''}     
-        customer['name']=input('이름을 입력하세요 : ')
+    customer={'name':'','sex':'',"email":'',"birthyear":''}     
+    customer['name']=input('이름을 입력하세요 : ')
+    while True:
+        customer['sex']=input("성별(M/m 또는 F/f)을 입력하세요").upper()
+        if customer['sex'] in ('m','f'):
+            break
+    
+    while True:         #중복되지 않게 입력
+        regex=re.compile('^[a-z][a-z0-9]{4,10}@[a-zA-Z]{2,6}[.][a-zA-Z{2,3}$')
         while True:
-            customer['sex']=input("성별(M/m 또는 F/f)을 입력하세요").upper()
-            if customer['sex'] in ('m','f'):
+            customer['email']=input('이메일을 입력하세요 : ')
+            golbang=regex.search(customer['email'])
+            if golbang:
                 break
-        
-        while True:         #중복되지 않게 입력
-            regex=re.compile('^[a-z][a-z0-9]{4,10}@[a-zA-Z]{2,6}[.][a-zA-Z{2,3}$')
-            while True:
-                customer['email']=input('이메일을 입력하세요 : ')
-                golbang=regex.search(customer['email'])
-                if golbang:
-                    break
-                else:
-                    print('"@"를 포함한 정확한 이메일을 써주세요')      #형식
+            else:
+                print('"@"를 포함한 정확한 이메일을 써주세요')      #형식
 
-            check=0
-            for i in custlist:
-                if i['email']==customer['email']:
-                    check=1
-            if check==0:
-                print('중복되는 이메일이 있습니다.')                    #중복
-            while True:
-                customer['birthday']=input("출생년도 4자리를 입력해 주세여 : ")
-                if len(customer['birthday'])==4 and customer['birthday'].isdigit():
-                    break
+        check=0
+        for i in custlist:
+            if i['email']==customer['email']:
+                check=1
+        if check==0:
+            print('중복되는 이메일이 있습니다.')                    #중복
+        while True:
+            customer['birthday']=input("출생년도 4자리를 입력해 주세여 : ")
+            if len(customer['birthday'])==4 and customer['birthday'].isdigit():
+                break
         print(customer)
         custlist.append(customer)
         print(custlist)
@@ -140,16 +117,3 @@ def loadData():
         with open('basic/data.pkl','rb') as f:
             custlist=pickle.load(f)
             page=len(custlist)-1
-       
-while True:
-    choice=input('''
-    다음 중에서 하실 일을 골라주세요 :
-    I - 고객 정보 입력
-    C - 현재 고객 정보 조회
-    P - 이전 고객 정보 조회
-    N - 다음 고객 정보 조회
-    U - 고객 정보 수정
-    D - 고객 정보 삭제
-    Q - 프로그램 종료
-    ''').upper()  
-    exe(choice)
